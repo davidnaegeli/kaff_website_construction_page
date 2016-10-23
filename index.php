2ebdd6d85a2110e687efc1c85d81d9bd7c3b2208
KaffHub <can_cinar@me.com>
2016-10-23 22:50:28 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,103 @@
+<html>
+<head>
+    <!--Meta Data -->
+    <title>Projekt KAFF: Kulturarbeit für Frauenfeld</title>
+    <link rel="icon" href="images/kaff.ico">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <!--External-Import -->
+
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js"
+            integrity="sha256-8WqyJLuWKRBVhxXIL1jBDD7SDxU936oZkCnxQbWwJVw="
+            crossorigin="anonymous"></script>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="center">
+    <img src="images/construction_small.png">
+
+
+    <h3 class="slide__text-heading">
+        kaff.ch befindet sich zur Zeit im Umbau. Sie wird aber bald und besser wieder da sein. Solnage, gibts hier ganz
+        simpel Programm und Kontakt zu sehen
+    </h3>
+
+</div>
+
+<div class="center">
+    <img src="images/KAFF_Programm_Oktober_Seite2.jpg">
+</div>
+
+<div class="center">
+    <h3 class="slide__text-heading">
+        Kontakangaben
+    </h3>
+    <ul class="slide__text-desc list-unstyled">
+        <li>
+            <b>Kulturlokal:</b> Grabensrasse 57, 8500 Fraunfeld
+        </li>
+        <li>
+            <b>Vereinshaus:</b> Reihnstrasse 14, 8500 Frauenfeld
+        </li>
+        <li>
+            <b>Info-Mail:</b> <a>info@kaff.ch</a>
+        </li>
+        <li>
+            <b>Schicht-Mail:</b> <a>schicht@kaff.ch</a>
+        </li>
+        <li>
+            <b>Band-Mail:</b> <a>bands@kaff.ch</a>
+        </li>
+    </ul>
+    <h3 class="slide__text-heading">
+        Partner, Sponsoren und weitere KAFF-Projekte
+    </h3>
+    <ul class="slide__text-desc">
+        <li>
+            Kanton Thurgau
+        </li>
+        <li>
+            Stadt Frauenfeld
+        </li>
+        <li>
+            KÄPT'N
+        </li>
+        <li>
+            Out In The Green Garden (OITGG)
+        </li>
+        <li>
+            Autonome Schule Frauenfeld (ASF)
+        </li>
+    </ul>
+</div>
+
+
+<style>
+    .center {
+        display: table;
+        width: 60%;
+        margin: auto;
+        margin-bottom: 20px;
+    }
+
+    .center img {
+        display: block;
+        margin: auto;
+    }
+
+    @media (max-width: 991px) {
+        .center {
+            display: table;
+            width: 80%;
+        }
+    }
+
+</style>
+
+</body>
+
+</html>
